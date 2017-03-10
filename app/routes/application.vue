e8683fdd7cc971951b2cfec84853c67c87d49f42
Kate Leslie <thisizk8@gmail.com>
2017-03-10 10:35:52 -0600

    Installing redux, thunk, revue, & lodash

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -3,63 +3,52 @@
 
     <nav class="nav">
       <div class="nav-left">
-          <h1><a href="/" class="nav-item is-active">Puppies</a></h1>
+          <router-link class="nav-item is-brand" :to="{ name: 'index' }">Puppies</router-link>
       </div>
       <div class="nav-right">
-        <a href="/" class="nav-item">All Puppies</a>
-        <a href="/new" class="nav-item">Add Puppy</a>
+        <router-link class="nav-item" :to="{ name: 'index' }">All Puppies</router-link>
+        <router-link class="nav-item" :to="{ name: 'new' }">Add Puppy</router-link>
       </div>
     </nav>
 
-    <aside class="menu">
-      <h5 class="menu-label">Adopt a pupper</h5>
-      <ul class="menu-list">
-        <li class="media">
-          <div class="media-left">
-            <img src="http://lorempixel.com/75/75/cats/" alt="">
-          </div>
-          <div class="media-content">
-            <h1>Dog Name</h1>
-            <a href="/detail">Read more</a>
-          </div>
-        </li>
-        <li class="media">
-          <div class="media-left">
-            <img src="http://lorempixel.com/75/75/cats/" alt="">
-          </div>
-          <div class="media-content">
-            <h1>Dog Name</h1>
-            <a href="/detail">Read more</a>
-          </div>
-        </li>
-        <li class="media">
-          <div class="media-left">
-            <img src="http://lorempixel.com/75/75/cats/" alt="">
-          </div>
-          <div class="media-content">
-            <h1>Dog Name</h1>
-            <a href="/detail">Read more</a>
-          </div>
-        </li>
-        <li class="media">
-          <div class="media-left">
-            <img src="http://lorempixel.com/75/75/cats/" alt="">
-          </div>
-          <div class="media-content">
-            <h1>Dog Name</h1>
-            <a href="/detail">Read more</a>
-          </div>
-        </li>
-      </ul>
-    </aside>
+    <div class="section">
+      <div class="container">
+        <ul class="panel">
+          <li class="panel-heading">Adopt a pupper</li>
+          <li class="panel-block">
+            <div class="media">
+              <div class="media-left">
+                <img src="http://lorempixel.com/75/75/cats/" alt="">
+              </div>
+              <div class="media-content">
+                <h1>Dog Name</h1>
+                <a href="/detail">Read more</a>
+              </div>
+            </div>
+          </li>
+          <li class="panel-block">
+            <div class="media">
+              <div class="media-left">
+                <img src="http://lorempixel.com/75/75/cats/" alt="">
+              </div>
+              <div class="media-content">
+                <h1>Dog Name</h1>
+                <a href="/detail">Read more</a>
+              </div>
+            </div>
+          </li>
 
-    <main>
-      <transition>
-        <keep-alive>
-          <router-view></router-view>
-        </keep-alive>
-      </transition>
-    </main>
+        </ul>
+
+        <main>
+          <transition>
+            <keep-alive>
+              <router-view></router-view>
+            </keep-alive>
+          </transition>
+        </main>
+      </div>
+    </div>
 
 
 
--- app/routes/new.vue
+++ app/routes/new.vue
@@ -1,15 +1,16 @@
 <template lang="html">
   <div class="">
-    <form v-model="formValues">
-      <input type="text" name="name">
-      <input type="text" age="age">
-      <select name="sex">
+    <form >
+      <h2>{{formValues.name}}</h2>
+      <input type="text" name="name" v-model="formValues.name">
+      <input type="text" name="age" v-model="formValues.age">
+      <select name="sex" v-model="formValues.sex">
         <option value="female">Female</option>
         <option value="male">Male</option>
       </select>
-      <input type="text" color="color">
-      <input type="text" breed="breed">
-      <input type="text" image="image_url">
+      <input type="text" name="color" v-model="formValues.color">
+      <input type="text" name="breed" v-model="formValues.breed">
+      <input type="text" name="image_url" v-model="formValues.image_url">
       <textarea description="description" rows="8" cols="80">Description</textarea>
     </form>
   </div>
@@ -19,7 +20,14 @@
 export default {
   data() {
     return {
-      formValues: this.$select('formValues'),
+      formValues: {
+        name: '',
+        age: '',
+        sex: '',
+        color: '',
+        breed: '',
+        image: '',
+      },
     };
   },
 
